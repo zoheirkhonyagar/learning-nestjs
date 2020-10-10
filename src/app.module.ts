e303d91f67f65be3960c095a7740db1f1f28c9df
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2020-10-10 12:32:08 +0330

    feat: added config module to root of project

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
